e1432b001b0fb0457ed14060e68ab07561eb6852
Stepan <pris.stepa@mail.ru>
2023-06-13 20:19:29 +0300

    Исправил ошибку в сервисе комментария

--- src/main/java/ru/skypro/homework/service/CommentService.java
+++ src/main/java/ru/skypro/homework/service/CommentService.java
@@ -33,8 +33,8 @@ public class CommentService {
 
         List<Comment> commentDtoList = new ArrayList<>();
 
-        for (int i = 0; i < commentEntityList.size()+1; i++) {
-            Comment thisComment = CommentMapper.INSTANCE.commentEntityToComment(commentEntityList.get(i));
+        for (CommentEntity commentEntity: commentEntityList) {
+            Comment thisComment = CommentMapper.INSTANCE.commentEntityToComment(commentEntity);
             commentDtoList.add(thisComment);
         }
 
