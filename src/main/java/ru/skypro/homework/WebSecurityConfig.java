45260cf011a7053846293410493fdc1578833896
Stepan <pris.stepa@mail.ru>
2023-07-09 11:23:49 +0300

    Исправил создание комментария и веб конфиг

--- src/main/java/ru/skypro/homework/WebSecurityConfig.java
+++ src/main/java/ru/skypro/homework/WebSecurityConfig.java
@@ -1,21 +1,15 @@
 package ru.skypro.homework;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sql.DataSource;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 //@Configuration
 @EnableWebSecurity
@@ -46,7 +40,7 @@ public class WebSecurityConfig {
                 )
                 .cors()
                 .and()
-                .httpBasic(withDefaults());
+                .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).httpBasic(withDefaults());
         return http.build();
     }
 
