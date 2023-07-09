6cbfbc75f5bbd85569239bf00aafb5fecc6ee95c
Anatoly <kjluh@mail.ru>
2023-07-09 09:34:44 +0300

    часть тестов готова

--- src/test/java/ru/skypro/homework/controller/UserControllerTest.java
+++ src/test/java/ru/skypro/homework/controller/UserControllerTest.java
@@ -37,7 +37,6 @@ public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
     private JSONObject testUserDTOJSON = new JSONObject();
-    private JSONObject testLoginJSON = new JSONObject();
     @Autowired
     private WebApplicationContext webApplicationContext;
 
@@ -49,18 +48,14 @@ public class UserControllerTest {
         testUserDTOJSON.put("lastName", "lastName");
         testUserDTOJSON.put("phone", "+79998887766");
         testUserDTOJSON.put("role", "ADMIN");
-
-    }
-
-    private void setTestLoginJSON() throws JSONException {
-        testLoginJSON.put("password", "password");
-        testLoginJSON.put("username", "username");
     }
 
     @Test
     void testReg() throws Exception {
         setTestUserDTOJSON();
-        setTestLoginJSON();
+        JSONObject testLoginJSON = new JSONObject();
+        testLoginJSON.put("password", "password");
+        testLoginJSON.put("username", "username");
         mockMvc.perform(
                         post("/register").contentType(MediaType.APPLICATION_JSON).content(testUserDTOJSON.toString()))
                 .andExpect(status().isOk());
@@ -79,10 +74,6 @@ public class UserControllerTest {
 
     @Test
     void testGetUser() throws Exception {
-        setTestLoginJSON();
-        mockMvc.perform(
-                        post("/login").contentType(MediaType.APPLICATION_JSON).content(testLoginJSON.toString()))
-                .andExpect(status().isOk());
         mockMvc.perform(
                         get("/users/me")
                                 .header("Authorization", "Basic " +
@@ -92,14 +83,10 @@ public class UserControllerTest {
 
     @Test
     void testUpdateUser() throws Exception {
-        setTestLoginJSON();
         JSONObject testUserUpdateDTOJSON = new JSONObject();
         testUserUpdateDTOJSON.put("firstName", "firstName");
         testUserUpdateDTOJSON.put("lastName", "lastName");
         testUserUpdateDTOJSON.put("phone", "+79998887766");
-        mockMvc.perform(
-                        post("/login").contentType(MediaType.APPLICATION_JSON).content(testLoginJSON.toString()))
-                .andExpect(status().isOk());
         mockMvc.perform(
                         patch("/users/me").contentType(MediaType.APPLICATION_JSON).content(testUserUpdateDTOJSON.toString())
                                 .header("Authorization", "Basic " +
@@ -109,13 +96,9 @@ public class UserControllerTest {
 
     @Test
     void testUpdateUserPassword() throws Exception {
-        setTestLoginJSON();
         JSONObject testNewPasswordJSON = new JSONObject();
         testNewPasswordJSON.put("currentPassword", "password");
         testNewPasswordJSON.put("newPassword", "password111");
-        mockMvc.perform(
-                        post("/login").contentType(MediaType.APPLICATION_JSON).content(testLoginJSON.toString()))
-                .andExpect(status().isOk());
         mockMvc.perform(
                         post("/users/set_password").contentType(MediaType.APPLICATION_JSON).content(testNewPasswordJSON.toString())
                                 .header("Authorization", "Basic " +
@@ -130,17 +113,21 @@ public class UserControllerTest {
 
     @Test
     void testLoadAvatar() throws Exception {
-        setTestLoginJSON();
-        MockMultipartFile file = new MockMultipartFile("file", "hello.png", MediaType.IMAGE_PNG_VALUE, "Hello, World!".getBytes());
+        MockMultipartFile image = new MockMultipartFile("image", "hello.png", MediaType.IMAGE_PNG_VALUE, "Hello, World!".getBytes());
 
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
         mockMvc.perform(
-                        post("/login").contentType(MediaType.APPLICATION_JSON).content(testLoginJSON.toString()))
-                .andExpect(status().isOk());
-
-        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-        mockMvc.perform(multipart("/users/me/image").file(file)
-                        .header("Authorization", "Basic " +
-                                Base64.getEncoder().encodeToString(("username" + ":" + "password").getBytes(StandardCharsets.UTF_8))))
+                        multipart("/users/me/image").file(image)
+                                .header("Authorization", "Basic " +
+                                        Base64.getEncoder().encodeToString(("username" + ":" + "password").getBytes(StandardCharsets.UTF_8))))
                 .andExpect(status().isOk());
     }
+//    @Test
+//    void testGetAvatar() throws Exception {
+//        MockMultipartFile image = new MockMultipartFile("image", "hello.png", MediaType.IMAGE_PNG_VALUE, "Hello, World!".getBytes());
+//
+//        mockMvc.perform(
+//                        get("/users/avatar/1/db").content(image.getBytes()))
+//                .andExpect(status().isOk());
+//    }
 }
